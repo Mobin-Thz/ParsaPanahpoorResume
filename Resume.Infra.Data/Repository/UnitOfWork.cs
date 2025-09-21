e97cbb4bee03ce3d9705b0d103c554f132d1f511
mobin-thz <mobintaherzadeh@gmail.com>
2025-09-21 19:39:32 +0330

    UpdateUnitOfWork

---
+++ Resume.Infra.Data/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Resume.Application.Common.Interfaces;
+using Resume.Domain.Entity.Reservation;
+using Resume.Infra.Data.MongoDb;
+using Resume.Infra.Data.SQLServer.Context;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Infra.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly PostgresDbContext _postgresqlDbContext;
+
+        public UnitOfWork(AppDbContext dbContext, PostgresDbContext postgresqlDbContext)
+        {
+            _dbContext = dbContext;
+            _postgresqlDbContext = postgresqlDbContext;
+        }
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            await _postgresqlDbContext.SaveChangesAsync();
+
+            return await _dbContext.SaveChangesAsync();
+            
+
+        }
+    }
+}
